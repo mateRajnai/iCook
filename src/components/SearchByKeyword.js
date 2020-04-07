a8f0568832cde163fc3039db8b5bb81696117d08
sandormatyas <sandormatyas7@gmail.com>
2020-04-07 22:02:42 +0200

    remove indexing from Search component

--- src/components/SearchByKeyword.js
+++ src/components/SearchByKeyword.js
@@ -3,9 +3,8 @@ import { Input } from "antd";
 import { RecipeContext } from "../context/RecipeContext";
 
 const SearchByKeyword = () => {
-  const SEARCH_FUNCTION_INDEX = 0;
   const { Search } = Input;
-  const { search } = useContext(RecipeContext)[SEARCH_FUNCTION_INDEX];
+  const { search } = useContext(RecipeContext);
 
   return (
     <div>
