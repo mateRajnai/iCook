906621885c8e7405beb4d42acdbb9d4c732b730d
sandormatyas <sandormatyas7@gmail.com>
2020-04-07 21:59:00 +0200

    add search form to RecipeContent

--- src/components/RecipeContent.js
+++ src/components/RecipeContent.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Row, Col } from "antd";
 import RecipeFilter from "./RecipeFilter";
 import RecipeList from "./RecipeList";
+import SearchByKeyword from "./SearchByKeyword";
 
 export const RecipeContent = (props) => {
   return (
     <Row>
-      <Col lg={8} md={6} sm={6}>
-        <RecipeFilter />
+      <Col lg={6} md={4} sm={4}>
+        <Row>
+          <RecipeFilter />
+        </Row>
+        <Row>
+          <SearchByKeyword />
+        </Row>
       </Col>
-      <Col lg={16} md={18} sm={18}>
+      <Col lg={18} md={20} sm={20}>
         <RecipeList />
       </Col>
     </Row>
