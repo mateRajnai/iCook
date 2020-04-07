bf5dbf85f16423e2d9d415cf642344402b5d9c27
Rajnai Máté <ramate.rajnai@gmail.com>
2020-04-07 11:47:02 +0200

    Send request to api endpoint

--- src/components/SearchByKeyword.js
+++ src/components/SearchByKeyword.js
@@ -1,14 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Input } from "antd";
+import Axios from "axios";
 
 const SearchByKeyword = () => {
   const { Search } = Input;
+
+  const API_ID = "29f808e6";
+  const API_KEY = "172c8533603f02665a8920e3ee1ea944";
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const searchRecipes = (value) => {
+    Axios.get(
+      `https://api.edamam.com/search?q=${value}&app_id=${API_ID}&app_key=${API_KEY}`
+    ).then((resp) => {
+      setFetchedData(resp);
+    });
+  };
+
+  console.log(fetchedData);
 
   return (
     <div>
       <Search
         placeholder="Search recipes by keyword"
-        onSearch={(value) => console.log(value)}
+        onSearch={(value) => searchRecipes(value)}
         enterButton
       />
     </div>
