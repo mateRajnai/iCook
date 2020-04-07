216312717faff5f9ffe5a1aadb22e41a13e63f5d
sandormatyas <sandormatyas7@gmail.com>
2020-04-07 23:52:59 +0200

    create RecipeDetails component

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { RecipeContext } from "../context/RecipeContext";
+
+const RecipeDetails = (props) => {
+  // const {selectedRecipe} = useContext(RecipeContext)
+
+  return <div></div>;
+};
+
+export default RecipeDetails;
