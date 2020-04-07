40d37449c73a43064d67d7b0408ef1a6c1c50c74
sandormatyas <sandormatyas7@gmail.com>
2020-04-07 22:01:58 +0200

    add more info to render to RecipeListItem

--- src/components/RecipeListItem.js
+++ src/components/RecipeListItem.js
@@ -10,54 +10,77 @@ const RecipeListItem = (props) => {
     console.log("click");
   };
 
+  const addToBookmarks = () => {
+    console.log("bookmarks");
+  };
+
   return (
     <ListItem>
-      <Col lg={16} md={24}>
-        <Card title={recipe.label} onClick={showRecipedetails}>
-          <Row gutter={[32]}>
-            <Col>
-              <img src={recipe.image} alt={recipe.label} height={100}></img>
-            </Col>
-            <Col>
-              <Row>Calories:</Row>
-              <Row>
-                <p>{parseFloat(recipe.calories).toFixed(2)}</p>
-              </Row>
-            </Col>
-            <Col>
-              <Row>Diet properties:</Row>
-              <Row>
-                <ul>
-                  {recipe.dietLabels.map((label) => (
-                    <li key={label}>{label}</li>
-                  ))}
-                </ul>
-              </Row>
-            </Col>
-            <Col>
-              <Row>Health properties:</Row>
-              <Row>
-                <ul>
-                  {recipe.healthLabels.map((label) => (
-                    <li key={label}>{label}</li>
-                  ))}
-                </ul>
-              </Row>
-            </Col>
-            <Col flex="auto">
-              <Row justify="center">
-                <Col>
-                  <FontAwesomeIcon
-                    id="bookmark"
-                    icon={faBookmark}
-                    size={"5x"}
-                  ></FontAwesomeIcon>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Card>
-      </Col>
+      <Row justify="center">
+        <Col sm={24} md={24} lg={18}>
+          <Card title={recipe.label} onClick={showRecipedetails}>
+            <Row gutter={[32]}>
+              <Col>
+                <img src={recipe.image} alt={recipe.label} height={100}></img>
+              </Col>
+              <Col>
+                <Row>Servings:</Row>
+                <Row>
+                  <p>{recipe.yield}</p>
+                </Row>
+              </Col>
+              <Col>
+                <Row>Calories:</Row>
+                <Row>
+                  <p>{parseFloat(recipe.calories).toFixed(2)}</p>
+                </Row>
+              </Col>
+              <Col>
+                <Row>Calories per serving:</Row>
+                <Row>
+                  <p>
+                    {(
+                      parseFloat(recipe.calories) / parseFloat(recipe.yield)
+                    ).toFixed(2)}
+                  </p>
+                </Row>
+              </Col>
+              <Col>
+                <Row>Diet properties:</Row>
+                <Row>
+                  <ul>
+                    {recipe.dietLabels.map((label) => (
+                      <li key={label}>{label}</li>
+                    ))}
+                  </ul>
+                </Row>
+              </Col>
+              <Col>
+                <Row>Health properties:</Row>
+                <Row>
+                  <ul>
+                    {recipe.healthLabels.map((label) => (
+                      <li key={label}>{label}</li>
+                    ))}
+                  </ul>
+                </Row>
+              </Col>
+              <Col flex="auto">
+                <Row justify="center">
+                  <Col>
+                    <FontAwesomeIcon
+                      onClick={addToBookmarks}
+                      id="bookmark"
+                      icon={faBookmark}
+                      size={"5x"}
+                    ></FontAwesomeIcon>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
     </ListItem>
   );
 };
