0a9fe9d32480cb5ee00d592ecb75c1ab443bb83c
sandormatyas <sandormatyas7@gmail.com>
2020-04-07 15:11:19 +0200

    move api call to RecipeContext

--- src/components/SearchByKeyword.js
+++ src/components/SearchByKeyword.js
@@ -1,26 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Input } from "antd";
-import Axios from "axios";
+import { RecipeContext } from "../RecipeContext";
 
 const SearchByKeyword = () => {
   const { Search } = Input;
-
-  const API_ID = "29f808e6";
-  const API_KEY = "172c8533603f02665a8920e3ee1ea944";
-
-  const searchRecipes = (value) => {
-    Axios.get(
-      `https://api.edamam.com/search?q=${value}&app_id=${API_ID}&app_key=${API_KEY}`
-    ).then((resp) => {
-      console.log(resp);
-    });
-  };
+  const { search } = useContext(RecipeContext);
 
   return (
     <div>
       <Search
         placeholder="Search recipes by keyword"
-        onSearch={(value) => searchRecipes(value)}
+        onSearch={(value) => search(value)}
         enterButton
       />
     </div>
