d53dfceed4a91bc3303a1466cc60d8f7ebb1dedb
sandormatyas <sandormatyas7@gmail.com>
2020-04-07 17:52:33 +0200

    add a styled-component wrapper to RecipeListItem

--- src/components/RecipeListItem.js
+++ src/components/RecipeListItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Row, Col } from "antd";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import ListItem from "../style/ListItem";
 
 const RecipeListItem = (props) => {
   const recipe = props.recipe;
@@ -10,59 +11,54 @@ const RecipeListItem = (props) => {
   };
 
   return (
-    <Col lg={16} md={24}>
-      <Card title={recipe.label} onClick={showRecipedetails}>
-        <Row gutter={[32]}>
-          <Col>
-            <img src={recipe.image} alt={recipe.label} height={100}></img>
-          </Col>
-          <Col>
-            <Row>Calories:</Row>
-            <Row>
-              <p>{parseFloat(recipe.calories).toFixed(2)}</p>
-            </Row>
-          </Col>
-          <Col>
-            <Row>Diet properties:</Row>
-            <Row>
-              <ul>
-                {recipe.dietLabels.map((label) => (
-                  <li key={label}>{label}</li>
-                ))}
-              </ul>
-            </Row>
-          </Col>
-          <Col>
-            <Row>Health properties:</Row>
-            <Row>
-              <ul>
-                {recipe.healthLabels.map((label) => (
-                  <li key={label}>{label}</li>
-                ))}
-              </ul>
-            </Row>
-          </Col>
-          <Col flex="auto">
-            <Row justify="center">
-              <Col>
-                <FontAwesomeIcon icon={faBookmark} size="lg"></FontAwesomeIcon>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Card>
-    </Col>
-
-    // <div>
-    //   <img src={props.recipe.image} alt={props.recipe.label}></img>
-    //   <span>{props.recipe.label}</span>
-    //   {props.recipe.dietLabels.map((label) => (
-    //     <li>{label}</li>
-    //   ))}
-    //   {props.recipe.healthLabels.map((label) => (
-    //     <li>{label}</li>
-    //   ))}
-    // </div>
+    <ListItem>
+      <Col lg={16} md={24}>
+        <Card title={recipe.label} onClick={showRecipedetails}>
+          <Row gutter={[32]}>
+            <Col>
+              <img src={recipe.image} alt={recipe.label} height={100}></img>
+            </Col>
+            <Col>
+              <Row>Calories:</Row>
+              <Row>
+                <p>{parseFloat(recipe.calories).toFixed(2)}</p>
+              </Row>
+            </Col>
+            <Col>
+              <Row>Diet properties:</Row>
+              <Row>
+                <ul>
+                  {recipe.dietLabels.map((label) => (
+                    <li key={label}>{label}</li>
+                  ))}
+                </ul>
+              </Row>
+            </Col>
+            <Col>
+              <Row>Health properties:</Row>
+              <Row>
+                <ul>
+                  {recipe.healthLabels.map((label) => (
+                    <li key={label}>{label}</li>
+                  ))}
+                </ul>
+              </Row>
+            </Col>
+            <Col flex="auto">
+              <Row justify="center">
+                <Col>
+                  <FontAwesomeIcon
+                    id="bookmark"
+                    icon={faBookmark}
+                    size={"5x"}
+                  ></FontAwesomeIcon>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+    </ListItem>
   );
 };
 
---
+++ src/style/ListItem.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default styled.div`
+  & * :hover {
+    background-color: #cbffcb;
+  }
+
+  & * svg :hover {
+    color: #005500;
+    cursor: pointer;
+  }
+`;
