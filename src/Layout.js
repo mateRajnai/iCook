538ebfa3c6c4d3fd7b095209cf7c024a34e991b5
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-04-07 10:37:47 +0200

    Add basic layout of filtering component

--- src/Layout.js
+++ src/Layout.js
@@ -3,11 +3,17 @@ import { Layout as AntLayout } from "antd";
 import LayoutHeader from "./components/LayoutHeader";
 import LayoutFooter from "./components/LayoutFooter";
 import "antd/dist/antd.css";
+import RecipeFilter from "./components/RecipeFilter";
+
+const { Content } = AntLayout;
 
 const Layout = (props) => {
   return (
     <AntLayout className="layout">
       <LayoutHeader />
+      <Content style={{ margin: "150px 20px 100px 20px" }}>
+        <RecipeFilter />
+      </Content>
       <LayoutFooter />
     </AntLayout>
   );
