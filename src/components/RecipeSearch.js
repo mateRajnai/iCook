0f4a47ecf540d906679154f2d53f5752fc03d210
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-04-09 09:15:05 +0200

    Delete console log

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -165,7 +165,6 @@ const RecipeSearch = () => {
   const [healthLabelFilter, setHealthLabels] = useState(HEALTH_FILTER);
 
   const prepareFilterQuery = () => {
-    console.log(ingredientNumberFilter.queryString);
     return (
       ingredientNumberFilter.queryString +
       caloriesFilter.queryString +
