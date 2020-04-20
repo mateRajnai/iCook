9bbec9dd694cf13aaa516621f98ababfdf30bb4b
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-04-20 19:05:29 +0200

    Set API credentails as react env variables

--- src/components/RecipeSearchContent.js
+++ src/components/RecipeSearchContent.js
@@ -1,31 +1,31 @@
 import React from "react";
 import { Row, Col, Menu, Layout } from "antd";
+import styled from "styled-components";
 
 import RecipeSearch from "./RecipeSearch";
 import RecipeList from "./RecipeList";
 
 const { Sider } = Layout;
 
+const InnerDiv = styled.div`
+  position: fixed;
+  top: "70px";
+  padding-top: "20px";
+  width: "500px";
+  margin-left: "50px";
+`;
+
 export const RecipeContent = (props) => {
   return (
     <Row>
       <Col lg={6} md={4} sm={4}>
-        <div
-          className="ant-affix"
-          style={{
-            position: "fixed",
-            top: "70px",
-            paddingTop: "20px",
-            width: "500px",
-            marginLeft: "50px",
-          }}
-        >
+        <InnerDiv className="ant-affix">
           <Sider width={400} className="site-layout-background main-menu-inner">
             <Menu className="aside-container" mode="inline">
               <RecipeSearch />
             </Menu>
           </Sider>
-        </div>
+        </InnerDiv>
       </Col>
       <Col lg={18} md={20} sm={20}>
         <RecipeList />
