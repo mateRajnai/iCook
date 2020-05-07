9d5ceb3d1a587a2fb937fa60d9339d6a154a53e0
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-05-07 21:01:13 +0200

    Change personal note url a bit

--- src/context/PersonalNoteContext.js
+++ src/context/PersonalNoteContext.js
@@ -17,8 +17,7 @@ export const PersonalNoteProvider = (props) => {
     .toLowerCase()
     .replace(/ /g, "-");
 
-  const URLForList = `http://localhost:8080/recipe/${selectedRecipeId}/personal-note/list`;
-  const URLForSave = `http://localhost:8080/recipe/${selectedRecipeId}/personal-note/save`;
+  const URLForList = `http://localhost:8080/recipe/${selectedRecipeId}/personal-note`;
 
   const collectNewPersonalNoteRelatedData = () => {
     const newPersonalNote = document.getElementById(
@@ -43,7 +42,7 @@ export const PersonalNoteProvider = (props) => {
   const addPersonalNote = (event) => {
     const data = collectNewPersonalNoteRelatedData();
     event.stopPropagation();
-    Axios.post(URLForSave, data, {
+    Axios.post(URLForList, data, {
       headers: {
         "Content-Type": "application/json",
       },
