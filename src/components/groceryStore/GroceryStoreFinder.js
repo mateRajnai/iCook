8277b83ce25edc09eda0f998033f57873c71c20c
Rajnai Máté <ramate.rajnai@gmail.com>
2020-06-03 14:48:40 +0200

    Restrict search for grocery stores for users only

--- src/components/groceryStore/GroceryStoreFinder.js
+++ src/components/groceryStore/GroceryStoreFinder.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button } from "antd";
+import { UserContext } from "../../context/UserContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+import { Row, Typography } from "antd";
+import styled from "styled-components";
+
+const { Title } = Typography;
+
+const RowWithMargin = styled(Row)`
+  margin: 40px;
+`;
 
 const GroceryStoreFinder = () => {
+  const { isLoggedIn } = useContext(UserContext);
   const display = () => {
     console.log("coordinates");
     console.log(window.geolocation);
   };
 
-  return (
-    <div>
-      <Button onClick={display}>Search for grocery stores nearby</Button>
-    </div>
-  );
+  let content = null;
+
+  if (isLoggedIn) {
+    content = (
+      <div>
+        <Button onClick={display}>Search for grocery stores nearby</Button>
+      </div>
+    );
+  } else {
+    content = (
+      <React.Fragment>
+        <RowWithMargin justify="center">
+          <FontAwesomeIcon
+            icon={faExclamationCircle}
+            spin
+            size="8x"
+            style={{ color: "orange" }}
+          ></FontAwesomeIcon>
+        </RowWithMargin>
+        <RowWithMargin justify="center">
+          <Title level={4}>
+            Please log in first to search for grocery stores nearby.
+          </Title>
+        </RowWithMargin>
+      </React.Fragment>
+    );
+  }
+  return content;
 };
 
 export default GroceryStoreFinder;
